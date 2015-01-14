a0cfb7cc5d5fb8da78e611ead4bfed6b2f602433
Arjit Chaudhary <arjit.c@gmail.com>
2015-01-15 03:06:35 +0530

    Added receive and transmit bandwidth usage.

--- infinity.sh
+++ infinity.sh
@@ -26,6 +26,13 @@ ping_us=$(ping -c 1 184.105.253.14 | tail -1| awk -F '/' '{print $5}')
 ping_eu=$(ping -c 1 216.66.80.30 | tail -1| awk -F '/' '{print $5}')
 ping_asia=$(ping -c 1 216.218.221.42 | tail -1| awk -F '/' '{print $5}')
 
+receive_1=$(cat /proc/net/dev | grep eth0 | awk {'print $2'})
+transmit_1=$(cat /proc/net/dev | grep eth0 | awk {'print $10'})
+sleep 1
+receive_2=$(cat /proc/net/dev | grep eth0 | awk {'print $2'})
+transmit_2=$(cat /proc/net/dev | grep eth0 | awk {'print $10'})
+receive='expr $receive_2 - $receive_1'
+transmit='expr $transmit_2 - $transmit_1'
 #OS
 os_version=$(cat /etc/redhat-release)
 if [ -z "$os_version" ]
@@ -34,4 +41,4 @@ if [ -z "$os_version" ]
 fi
 
 ##Lets post the data
-curl --data "server_key=$server_key&ram_total=$ram_total&ram_free=$ram_free&cpu_name=$cpu_name&uptime=$uptime&load_1=$load_1&load_2=$load_2&load_3=$load_3&iowait=$iowait&ping_us=$ping_us&ping_eu=$ping_eu&ping_asia=$ping_asia&system_cpu=$system_cpu&idle_cpu=$idle_cpu&user_cpu=$user_cpu" http://infinitystat.com/infinity.php
+curl --data "server_key=$server_key&ram_total=$ram_total&ram_free=$ram_free&cpu_name=$cpu_name&uptime=$uptime&load_1=$load_1&load_2=$load_2&load_3=$load_3&iowait=$iowait&ping_us=$ping_us&ping_eu=$ping_eu&ping_asia=$ping_asia&system_cpu=$system_cpu&idle_cpu=$idle_cpu&user_cpu=$user_cpu&receive=$receive&transmit=$transmit" http://infinitystat.com/infinity.php
