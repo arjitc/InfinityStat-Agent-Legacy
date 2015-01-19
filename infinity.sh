14b7b048137eeb93f993c37a6be91359c9b62921
Arjit Chaudhary <arjit.c@gmail.com>
2015-01-19 22:58:31 +0530

    Added CPU fequency

--- infinity.sh
+++ infinity.sh
@@ -13,6 +13,7 @@ ram_free=$(awk '/MemFree/ {printf( "%.2f\n", $2 / 1024 )}' /proc/meminfo)
 ##CPU
 cpu_name=$(grep 'model name' /proc/cpuinfo | cut -d: -f2 |cut -d@ -f1 |head -1)
 cpu_usage_percentage=$(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage}')
+cpu_freq=$(cat /proc/cpuinfo | grep MHz | tail -n1 | awk '{print $4}')
 load_1=$(uptime | sed 's/.*load average: //' | awk -F\, '{print $1}')
 load_2=$(uptime | sed 's/.*load average: //' | awk -F\, '{print $2}')
 load_3=$(uptime | sed 's/.*load average: //' | awk -F\, '{print $3}')
@@ -48,4 +49,4 @@ if [ ! -r "$os_version_file" ]
 fi
 
 ##Lets post the data
-curl --data "server_key=$server_key&kernel_info=$kernel_info&ram_total=$ram_total&ram_free=$ram_free&cpu_name=$cpu_name&uptime=$uptime&load_1=$load_1&load_2=$load_2&load_3=$load_3&iowait=$iowait&ping_us=$ping_us&ping_eu=$ping_eu&ping_asia=$ping_asia&system_cpu=$system_cpu&idle_cpu=$idle_cpu&user_cpu=$user_cpu&receive=$receive&transmit=$transmit" http://infinitystat.com/infinity.php
+curl --data "server_key=$server_key&kernel_info=$kernel_info&ram_total=$ram_total&ram_free=$ram_free&cpu_freq=$cpu_freq&cpu_name=$cpu_name&uptime=$uptime&load_1=$load_1&load_2=$load_2&load_3=$load_3&iowait=$iowait&ping_us=$ping_us&ping_eu=$ping_eu&ping_asia=$ping_asia&system_cpu=$system_cpu&idle_cpu=$idle_cpu&user_cpu=$user_cpu&receive=$receive&transmit=$transmit" http://infinitystat.com/infinity.php
