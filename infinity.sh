7113566cd50b9eff7f146591dc803a14c4b4ed64
Arjit Chaudhary <arjit.c@gmail.com>
2015-02-12 00:43:26 +0530

    Added versioning.

--- infinity.sh
+++ infinity.sh
@@ -14,7 +14,8 @@ while true
 do
 	## Initial information
 	server_key=$(cat /etc/infinitystat.conf)
-
+	version="1"
+	
 	## Kernel info
 	kernel_info=$(uname -r)
 
@@ -66,7 +67,7 @@ do
 	process_count=$(ps aux | wc -l)
 
 	##Lets post the data
-	curl --data "server_key=$server_key&kernel_info=$kernel_info&ram_total=$ram_total&ram_free=$ram_free&ram_cached=$ram_cached&ram_buffers=$ram_buffers&cpu_freq=$cpu_freq&cpu_name=$cpu_name&cpu_count=$cpu_count&core_count=$core_count&uptime=$uptime&load_1=$load_1&load_2=$load_2&load_3=$load_3&iowait=$iowait&ping_us=$ping_us&ping_eu=$ping_eu&ping_asia=$ping_asia&system_cpu=$system_cpu&idle_cpu=$idle_cpu&user_cpu=$user_cpu&receive=$receive&transmit=$transmit&pps_receive=$pps_receive&pps_transmit=$pps_transmit&distro=$distro&process_count=$process_count" http://infinitystat.com/infinity.php
+	curl --data "server_key=$server_key&version=$version&kernel_info=$kernel_info&ram_total=$ram_total&ram_free=$ram_free&ram_cached=$ram_cached&ram_buffers=$ram_buffers&cpu_freq=$cpu_freq&cpu_name=$cpu_name&cpu_count=$cpu_count&core_count=$core_count&uptime=$uptime&load_1=$load_1&load_2=$load_2&load_3=$load_3&iowait=$iowait&ping_us=$ping_us&ping_eu=$ping_eu&ping_asia=$ping_asia&system_cpu=$system_cpu&idle_cpu=$idle_cpu&user_cpu=$user_cpu&receive=$receive&transmit=$transmit&pps_receive=$pps_receive&pps_transmit=$pps_transmit&distro=$distro&process_count=$process_count" http://infinitystat.com/infinity.php
 
 sleep 300
 done
