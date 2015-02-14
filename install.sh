b3aafec393d58159217f45498985007e2a24e611
Arjit Chaudhary <arjit.c@gmail.com>
2015-02-14 09:10:57 +0530

    Re-wrote bits of the installer for better dependency installs
    Re-wrote bits of the installer for better dependency installs via apt-get or yum.

--- install.sh
+++ install.sh
@@ -7,41 +7,19 @@ if [ -z "$install_key" ];
     exit 0
 fi
 ## install key is set, lets proceed with install of curl to post data and sysstat to get some I/O data
-if [ -z "$distro" ]
-        then
-            ##assuming distro has yum support centos/fedora/scientific
-            echo "Installing sysstat and curl packages"
-            yum install curl sysstat -y
-            wget https://raw.githubusercontent.com/arjitc/InfinityStat/master/infinitystat -O /etc/init.d/infinitystat --no-check-certificate
-            chmod +x /etc/init.d/infinitystat
-            wget https://raw.githubusercontent.com/arjitc/InfinityStat/master/infinity.sh -O /bin/infinitystat --no-check-certificate
-            chmod +x /bin/infinitystat
-            touch /etc/infinitystat.conf
-            echo "$1" >> /etc/infinitystat.conf
-            echo "Install complete"
-            echo "To start InfinityStat: service infinitystat start"
-            echo "To stop InfinityStat: service infinitystat stop"
-            echo "===="
-            echo "Starting InfinityStat service"
-            /etc/init.d/infinitystat start
-            echo "===="
-            
-        else
-            ##assuming distro has apt-get support / debian / ubuntu
-            sudo apt-get update
-            echo "Installing sysstat and curl packages"
-            sudo apt-get install sysstat curl -y
-            wget https://raw.githubusercontent.com/arjitc/InfinityStat/master/infinitystat -O /etc/init.d/infinitystat --no-check-certificate
-            chmod +x /etc/init.d/infinitystat
-            wget https://raw.githubusercontent.com/arjitc/InfinityStat/master/infinity.sh -O /bin/infinitystat --no-check-certificate
-            chmod +x /bin/infinitystat
-            touch /etc/infinitystat.conf
-            echo "$1" >> /etc/infinitystat.conf
-            echo "Install complete"
-            echo "To start InfinityStat: service infinitystat start"
-            echo "To stop InfinityStat: service infinitystat stop"
-            echo "===="
-            echo "Starting InfinityStat service"
-            /etc/init.d/infinitystat start
-            echo "===="
-fi
+echo "Installing sysstat and curl packages"
+apt-get update || yum update -y
+apt-get install sysstat curl -y || yum install sysstat curl -y
+wget https://raw.githubusercontent.com/arjitc/InfinityStat/master/infinitystat -O /etc/init.d/infinitystat --no-check-certificate
+chmod +x /etc/init.d/infinitystat
+wget https://raw.githubusercontent.com/arjitc/InfinityStat/master/infinity.sh -O /bin/infinitystat --no-check-certificate
+chmod +x /bin/infinitystat
+touch /etc/infinitystat.conf
+echo "$1" >> /etc/infinitystat.conf
+echo "Install complete"
+echo "To start InfinityStat: service infinitystat start"
+echo "To stop InfinityStat: service infinitystat stop"
+echo "===="
+echo "Starting InfinityStat service"
+/etc/init.d/infinitystat start
+echo "===="
