d50904a69f1697cb2417dd84197547958bfc514e
Arjit Chaudhary <arjit.c@gmail.com>
2015-04-03 16:07:43 +0530

    Added chkconfig dependency, start on boot
    Added chkconfig dependency, start infinitystat on boot

--- install.sh
+++ install.sh
@@ -9,13 +9,14 @@ fi
 ## install key is set, lets proceed with install of curl to post data and sysstat to get some I/O data
 echo "Installing sysstat and curl packages"
 apt-get update || yum update -y
-apt-get install sysstat curl -y || yum install sysstat curl -y
+apt-get install sysstat curl chkconfig -y || yum install sysstat curl chkconfig -y
 wget https://raw.githubusercontent.com/arjitc/InfinityStat/master/infinitystat -O /etc/init.d/infinitystat --no-check-certificate
 chmod +x /etc/init.d/infinitystat
 wget https://raw.githubusercontent.com/arjitc/InfinityStat/master/infinity.sh -O /bin/infinitystat --no-check-certificate
 chmod +x /bin/infinitystat
 touch /etc/infinitystat.conf
 echo "$1" >> /etc/infinitystat.conf
+chkconfig infinitystat on
 echo "Install complete"
 echo "To start InfinityStat: service infinitystat start"
 echo "To stop InfinityStat: service infinitystat stop"
